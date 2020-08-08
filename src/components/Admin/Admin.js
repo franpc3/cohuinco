c3d2c38256baf40b9aa385d7dd669c40181dbf33
Julio Cesar Ruiz Perez De Nucci <juliocrcr91@gmail.com>
2020-08-07 21:25:21 -0300

    changes

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -4,11 +4,13 @@ import Dashboard from './Dashboard';
 
 
 export default function Admin() {
-    /* const [login, setLogin] = useState(false); */
+     const [login, setLogin] = useState(false); 
     return (
         <div>
+            <h3 className="text-center mt-3">Administra los turnos </h3>
             <Dashboard />
            {/*  {login ? <Dashboard /> : <Login setLogin={setLogin} />} */}
+           
         </div>
     )
 }
--- src/components/Admin/Dashboard.js
+++ src/components/Admin/Dashboard.js
@@ -1,28 +1,103 @@
 import React from 'react'
-import { Container, Row, Col, Nav, Tab } from 'react-bootstrap';
+import { Container, Row, Col, Nav, Tab, Table, Form, Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {  faConciergeBell, faWater, faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import './Dashboard.css'
 
 export default function Dashboard() {
     return (
         <Container fluid>
-            <Tab.Container id="left-tabs-example" defaultActiveKey="first">
-                <Row>
+            <Tab.Container id="left-tabs-example" defaultActiveKey="first" >
+                <Row className="mt-5">
                     <Col sm={3}>
                         <Nav variant="pills" className="flex-column">
                             <Nav.Item>
-                                <Nav.Link eventKey="first">Rooms</Nav.Link>
+                                <Nav.Link eventKey="first"> <FontAwesomeIcon className="mr-2" icon={faConciergeBell} /> Rooms</Nav.Link>
                             </Nav.Item>
                             <Nav.Item>
-                                <Nav.Link eventKey="second">Rafting</Nav.Link>
+                                <Nav.Link eventKey="second">  <FontAwesomeIcon className="mr-2" icon={faWater} />Rafting</Nav.Link>
                             </Nav.Item>
                         </Nav>
                     </Col>
                     <Col sm={9}>
                         <Tab.Content>
                             <Tab.Pane eventKey="first">
-                                
+                                <Table responsive>
+                                    <thead>
+                                        <tr>
+                                            <th><Form.Check type="checkbox"/></th>
+                                            <th>Room Number</th>
+                                            <th>Floor</th>
+                                            <th>Description</th>
+                                            <th></th>
+                                            
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td><Form.Check type="checkbox"/></td>
+                                            <td>101</td>
+                                            <td>1</td>
+                                            <td>Simple room</td>
+                                            <td> <Button variant="warning"><FontAwesomeIcon  icon={faEdit} /></Button> <Button variant="danger"><FontAwesomeIcon  icon={faTrashAlt}/></Button></td>
+                                          
+                                        </tr>
+                                        <tr>
+                                            <td><Form.Check type="checkbox"/></td>
+                                            <td>102</td>
+                                            <td>1</td>
+                                            <td>Simple room</td>
+                                            <td> <Button variant="warning"><FontAwesomeIcon  icon={faEdit} /></Button> <Button variant="danger"><FontAwesomeIcon  icon={faTrashAlt}/></Button></td>
+                                           
+                                        </tr>
+                                        <tr>
+                                            <td><Form.Check type="checkbox"/></td>
+                                            <td>101</td>
+                                            <td>2</td>
+                                            <td>Family room</td>
+                                            <td> <Button variant="warning"><FontAwesomeIcon  icon={faEdit} /></Button> <Button variant="danger"><FontAwesomeIcon  icon={faTrashAlt}/></Button></td>
+                                           
+                                        </tr>
+                                    </tbody>
+                                </Table>
                             </Tab.Pane>
                             <Tab.Pane eventKey="second">
-                                
+                            <Table responsive>
+                                    <thead>
+                                        <tr>
+                                            <th><Form.Check type="checkbox"/></th>
+                                            <th>Room Number</th>
+                                            <th>Floor</th>
+                                            <th>Description</th>
+                                            <th></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td><Form.Check type="checkbox"/></td>
+                                            <td>101</td>
+                                            <td>1</td>
+                                            <td>Simple room</td>
+                                            <td> <Button variant="warning"><FontAwesomeIcon  icon={faEdit} /></Button> <Button variant="danger"><FontAwesomeIcon  icon={faTrashAlt}/></Button></td>
+                                          
+                                        </tr>
+                                        <tr>
+                                            <td><Form.Check type="checkbox"/></td>
+                                            <td>102</td>
+                                            <td>1</td>
+                                            <td>Simple room</td>
+                                            <td> <Button variant="warning"><FontAwesomeIcon  icon={faEdit} /></Button> <Button variant="danger"><FontAwesomeIcon  icon={faTrashAlt}/></Button></td>
+                                           
+                                        </tr>
+                                        <tr>
+                                            <td><Form.Check type="checkbox"/></td>
+                                            <td>101</td>
+                                            <td>2</td>
+                                            <td>Family room</td>
+                                            <td> <Button variant="warning"><FontAwesomeIcon  icon={faEdit} /></Button> <Button variant="danger"><FontAwesomeIcon  icon={faTrashAlt}/></Button></td>
+                                        </tr>
+                                    </tbody>
+                                </Table>
                             </Tab.Pane>
                         </Tab.Content>
                     </Col>
